14e07bc7125d5ca0f0836a53d5de0d21f73f0bac
beeeyan <moonsunphoenix@gmail.com>
2022-09-27 04:56:35 +0900

    firebaseの設定を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // firebaseの初期化
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
