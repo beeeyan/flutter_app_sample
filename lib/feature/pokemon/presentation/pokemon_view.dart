9d84fe1bdb9d1e1921fc2890dbc27533df9e58a4
beeeyan <moonsunphoenix@gmail.com>
2023-10-13 14:50:54 +0900

    gapの導入・pokemonAsyncの読み込み場所の変更

--- lib/feature/pokemon/presentation/pokemon_view.dart
+++ lib/feature/pokemon/presentation/pokemon_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../../common_widget/loading_widget.dart';
@@ -10,34 +11,35 @@ final pokemonFetchProvider = FutureProvider.autoDispose<Pokemon>((ref) async {
   return repository.fetch();
 });
 
-
 class PokemonViewPage extends ConsumerWidget {
   const PokemonViewPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
-   final pokemonAsync = ref.watch(pokemonFetchProvider);
+    final pokemonAsync = ref.watch(pokemonFetchProvider);
 
     return Scaffold(
       body: Center(
-        child: pokemonAsync.when(
-          data: (pokemon) => Column(
+        child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const Text(
                 'ポケモンの名前',
               ),
-              Text(
-                pokemon.name,
-                style: Theme.of(context).textTheme.headlineMedium,
+              const Gap(10),
+              pokemonAsync.when(
+                data: (pokemon) => Text(
+                  pokemon.name,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+                loading: () => loadingWidget,
+                error: (error, stackTrace) => Text(
+                  error.toString(),
+                ),
               ),
             ],
           ),
-          loading: () => loadingWidget,
-          error: (error, stackTrace) => Text(error.toString()),
         ),
-      ),// This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
